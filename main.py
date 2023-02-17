1b0473dcb81fd7448541fda23cf77a9c43584170
sonhun99 <57802358+sonhun99@users.noreply.github.com>
2023-02-17 15:22:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,51 +3,65 @@ import googleapiclient.discovery
 import googleapiclient.errors
 
 
-def reverse_playlist(youtube, playlist_id):
-    # Retrieve the videos in the original playlist
-    playlist_items = []
+def reverse_playlist(youtube, orig_playlist_id):
+    # Step 1: Create a new playlist with the opposite order
+    orig_playlist_response = (
+        youtube.playlists().list(part="snippet", id=orig_playlist_id).execute()
+    )
+    orig_playlist_title = orig_playlist_response["items"][0]["snippet"]["title"]
+    new_playlist_title = f"{orig_playlist_title} (reversed)"
+    new_playlist_description = "A reversed playlist"
+    new_playlist_response = (
+        youtube.playlists()
+        .insert(
+            part="snippet,status",
+            body={
+                "snippet": {
+                    "title": new_playlist_title,
+                    "description": new_playlist_description,
+                },
+                "status": {"privacyStatus": "private"},
+            },
+        )
+        .execute()
+    )
+    new_playlist_id = new_playlist_response["id"]
+
+    # Step 2: Get all the videos from the original playlist
+    videos = []
     next_page_token = None
     while True:
-        playlist_request = youtube.playlistItems().list(
-            part="snippet",
-            playlistId=playlist_id,
-            maxResults=50,
-            pageToken=next_page_token,
+        playlist_items_response = (
+            youtube.playlistItems()
+            .list(
+                part="snippet",
+                playlistId=orig_playlist_id,
+                maxResults=50,
+                pageToken=next_page_token,
+            )
+            .execute()
         )
-        playlist_response = playlist_request.execute()
-        playlist_items += playlist_response.get("items", [])
-        next_page_token = playlist_response.get("nextPageToken")
+
+        for item in playlist_items_response["items"]:
+            video_id = item["snippet"]["resourceId"]["videoId"]
+            print(video_id)  # Add this line to print the video ID
+            videos.append(video_id)
+
+        next_page_token = playlist_items_response.get("nextPageToken")
         if not next_page_token:
             break
 
-    # Create a new playlist with the opposite order
-    playlist_snippet = playlist_response.get("snippet", {})
-    new_playlist_request = youtube.playlists().insert(
-        part="snippet",
-        body={
-            "snippet": {
-                "title": "Reversed " + playlist_snippet.get("title", ""),
-                "description": playlist_snippet.get("description", ""),
-            }
-        },
-    )
-    new_playlist_response = new_playlist_request.execute()
-    new_playlist_id = new_playlist_response.get("id")
-
-    # Add the videos to the new playlist in reverse order
-    for item in reversed(playlist_items):
-        video_id = item.get("snippet", {}).get("resourceId", {}).get("videoId", "")
-        if video_id:
-            add_video_request = youtube.playlistItems().insert(
-                part="snippet",
-                body={
-                    "snippet": {
-                        "playlistId": new_playlist_id,
-                        "resourceId": {"kind": "youtube#video", "videoId": video_id},
-                    }
-                },
-            )
-            add_video_request.execute()
+    # Step 3: Add the videos to the new playlist in reverse order
+    for video_id in reversed(videos):
+        youtube.playlistItems().insert(
+            part="snippet",
+            body={
+                "snippet": {
+                    "playlistId": new_playlist_id,
+                    "resourceId": {"kind": "youtube#video", "videoId": video_id},
+                }
+            },
+        ).execute()
 
     print(
         "New playlist created with the opposite order: https://www.youtube.com/playlist?list="
